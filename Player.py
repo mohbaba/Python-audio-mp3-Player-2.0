b7e74e91a473de014076e778f7ab3956cb5d4a2a
mohbaba <babamu09@gmail.com>
2023-02-01 20:47:52 +0100

    App Container

---
+++ Player.py
@@ -0,0 +1,22 @@
+import flet
+from flet import *
+
+
+def main(page: Page):
+    page.title = "Audio Player"
+    
+    c = Container()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    page.add (Text ("Hello Fletizen!"))
+    
+
+flet.app(target=main, assets_dir="assets")
