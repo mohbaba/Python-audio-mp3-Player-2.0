c89078e5de9c5c2b2d6631685b5a6b30ad3df7b4
mohbaba <babamu09@gmail.com>
2023-02-04 23:16:33 +0100

    Side Bar

--- Player.py
+++ Player.py
@@ -16,10 +16,11 @@ def main(page: Page):
                 width = 330,
                 height= 500,
                 controls= [
-                    TextButton(
+                    ElevatedButton(
                         text = 'Tracks',
                         height = 50,
                         width= 320,
+                        color = 'black'
                         
                     )
                 ]
